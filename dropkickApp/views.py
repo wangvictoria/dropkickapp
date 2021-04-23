9b81399f1390b375bf1df4893560905b7f1d9576
wangvictoria <victoriawang29@gmail.com>
2021-04-23 11:47:18 -0500

    Initial commit

--- dropkickApp/views.py
+++ dropkickApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from dropkickApp.models import MyFile
 from django.views import generic
+from .forms import UploadFileForm, ModelFormWithFileField
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -16,3 +19,19 @@ def index(request):
     
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = ModelFormWithFileField(request.POST, request.FILES)
+        if form.is_valid():
+            # file is saved
+            form.save()
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = ModelFormWithFileField()
+    return render(request, 'upload.html', {'form': form})
---
+++ scripts/run_dropkick.py
@@ -0,0 +1,13 @@
+from dropkickApp.model import MyFile
+import scanpy as sc; sc.set_figure_params(color_map="viridis", frameon=False)
+import dropkick as dk
+
+def run():
+    sample = MyFile.objects.all()
+    
+    # read in counts data
+    adata = sc.read(sample)
+    
+    # plot QC metrics
+    adata = dk.recipe_dropkick(adata, n_hvgs=None, X_final="raw_counts")
+    qc_plt = dk.qc_summary(adata)
