dc64fcca04ab04250d6d9536b64f6d2592ee807f
wangvictoria <victoriawang29@gmail.com>
2021-07-01 15:39:23 -0500

    Fixed changes

--- dropkickApp/views.py
+++ dropkickApp/views.py
@@ -1,21 +1,12 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import MyFile
+from django.shortcuts import render, get_object_or_404
+from dropkickApp.models import MyFile
 from django.views import generic
 from .forms import UploadFileForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import os
-from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
-<<<<<<< Updated upstream
-=======
-# from dropkickApp.model import MyFile
-# import scanpy as sc; sc.set_figure_params(color_map="viridis", frameon=False)
-# import dropkick as dk
 
-
->>>>>>> Stashed changes
 # Create your views here.
 
 def index(request):
@@ -31,57 +22,18 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-<<<<<<< Updated upstream
-def handle_uploaded_file(f):
-    files = MyFile.objects.all()
-    return render(request, 'process.html', {'files: file'})
-
-=======
->>>>>>> Stashed changes
 def upload_file(request):
-    if request.method == 'POST':
-<<<<<<< Updated upstream
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('handle_uploaded_file')
-        else:
-            form = UploadFileForm()
-#     context = {}
-#     if request.method == 'POST':
-#         uploaded_file = request.FILES['upload']
-#         fs = FileSystemStorage()
-#         name = fs.save(uploaded_file.name, uploaded_file)
-#         context['url'] = fs.url(name)
-#         print(uploaded_file.name)
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#             # file is saved
-            
-#             #new = Storage()
-#             #new.file = request.FILES['file']
-#             #new.save()
-            
-#             #profile = Profile()
-#             #profile.name = MyProfileForm.cleaned_data["name"]
-#             #profile.picture = MyProfileForm.cleaned_data["picture"]
-#             #profile.save()
-#             #saved = True
-            
-#             #user_process = form.save(commit = False)
-#             #user_process.run_dropkick = request.FILES['upload']
-#             #return HttpResponseRedirect('/success/url/')
-#     else:
-#         form = UploadFileForm()
-    return render(request, 'index.html', { 'form': form })
+    # if request.method == 'POST':
+    #     my_form = UploadFileForm(request.POST, request.FILES)
+    #     if my_form.is_valid():
+    #         # file is saved
+    #         my_form.save()
+    #         return HttpResponseRedirect('/success/url/')
+    # else:
+    #     my_form = UploadFileForm()
+    # return render(request, 'upload.html', {'form': form})
 
-#def read(request):
-    #file = request.FILES[MyFile.name]
-    #df = pd.read_csv('file')
-    #file = open(os.path.join(settings.BASE_DIR, 'filename'))
-=======
+    if request.method == 'POST':
         uploaded_file = request.FILES['document']
         #print(uploaded_file.name)
         #print(uploaded_file.size)
@@ -95,4 +47,3 @@ def upload_file(request):
 #     adata = dk.recipe_dropkick(adata, n_hvgs=None, X_final="raw_counts")
 #     qc_plt = dk.qc_summary(adata)
 #     return render(request,'process.html')
->>>>>>> Stashed changes
--- scripts/run_dropkick.py
+++ scripts/run_dropkick.py
@@ -2,18 +2,7 @@ from dropkickApp.model import MyFile
 import scanpy as sc; sc.set_figure_params(color_map="viridis", frameon=False)
 import dropkick as dk
 
-<<<<<<< Updated upstream
 def run():
-    
-    #new = Storage()
-    #new.file = request.FILES['file']
-    #new.save()
-    
-    sample = MyFile.objects.all()
-    
-=======
-def run():    
->>>>>>> Stashed changes
     # read in counts data
     adata = sc.read("../media/3907_S1_jointcluster.h5ad")
     
