3340ab1103ea68589769628ee6713858e8499e2a
wangvictoria <victoriawang29@gmail.com>
2021-07-15 16:22:19 -0500

    Displays QC plot

--- dropkickApp/.ipynb_checkpoints/urls-checkpoint.py
+++ dropkickApp/.ipynb_checkpoints/urls-checkpoint.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload/', views.upload_file, name='upload'),
-    path('process/', views.process, name='process'),
+    #path('upload/', views.upload_file, name='upload'),
+    #path('process/', views.process, name='process'),
 ]
--- dropkickApp/.ipynb_checkpoints/views-checkpoint.py
+++ dropkickApp/.ipynb_checkpoints/views-checkpoint.py
@@ -2,53 +2,113 @@ from django.shortcuts import render, get_object_or_404
 from dropkickApp.models import MyFile
 from django.views import generic
 from .forms import UploadFileForm
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 
+import scanpy as sc; sc.set_figure_params(color_map="viridis", frameon=False)
+import dropkick as dk
+import matplotlib.pyplot as plt
+import io, base64, urllib
+import numpy as np
 
-# Create your views here.
+
+def qc_plot(adata):
+    # plot QC metrics
+    adata = dk.recipe_dropkick(adata, n_hvgs=None, X_final="raw_counts")
+    qc_plt = dk.qc_summary(adata)
+    
+    # display chart
+    buf = io.BytesIO()
+    qc_plt.savefig(buf, format = 'png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return uri
+
+def score_plot(adata):
+    score_plt = dk.score_plot(adata)
+    
+    # display chart
+    buf = io.BytesIO()
+    score_plt.savefig(buf, format = 'png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return uri
+
+def coef_plot(adata):
+    coef_plt = dk.coef_plot(adata)
+    
+    # display chart
+    buf = io.BytesIO()
+    coef_plt.savefig(buf, format = 'png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return uri
+
+def labels(adata):
+    adata_model = dk.dropkick(adata, n_jobs=5)
+    return adata_model
 
 def index(request):
     """View function for home page of site."""
     
-    # Generate count of files
-    num_files = MyFile.objects.all().count()
+    context = {'qc_plot': None, 'score_plot': None, 'coef_plot': None, 'labels': None}
     
-    context = {
-        'num_files': num_files,
-    }
-    
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
-
-def upload_file(request):
-    # if request.method == 'POST':
-    #     my_form = UploadFileForm(request.POST, request.FILES)
-    #     if my_form.is_valid():
-    #         # file is saved
-    #         my_form.save()
-    #         return HttpResponseRedirect('/success/url/')
-    # else:
-    #     my_form = UploadFileForm()
-    # return render(request, 'upload.html', {'form': form})
-
+    # upload file
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         #print(uploaded_file.name)
         #print(uploaded_file.size)
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
-    return render(request,'upload.html')
-
-def process(request):
-    model = MyFile
-    num_files = MyFile.objects.all().count()
     
-    context = {
-        'num_files': num_files,
-    }
-    return render(request, 'process.html')
+        # read data
+        adata = sc.read('media/' + uploaded_file.name)
+        
+        # run dropkick
+        #adata_model = dk.dropkick(adata, n_jobs=5)
+
+        # run scripts and create plots
+        
+        context['qc_plot'] = qc_plot(adata)
+        #context['score_plot'] = score_plot(adata)
+        #context['coef_plot'] = coef_plot(adata)
+        #context['labels'] = labels(adata)
+        
+    return render(request,'index.html', context)
+
+#     # Generate count of files
+#     num_files = MyFile.objects.all().count()
+    
+#     context = {
+#         'num_files': num_files,
+#     }
+    
+#     results = run_script()
+#     context['results'] = results
+    
+    # Render the HTML template index.html with the data in the context variable
+    #return render(request, 'index.html', context=context)
+
+# def upload_file(request):
+#     if request.method == 'POST':
+#         uploaded_file = request.FILES['document']
+#         #print(uploaded_file.name)
+#         #print(uploaded_file.size)
+#         fs = FileSystemStorage()
+#         fs.save(uploaded_file.name, uploaded_file)
+#     return render(request,'upload.html')
+
+# def process(request):
+#     model = MyFile
+#     num_files = MyFile.objects.all().count()
+    
+#     context = {
+#         'num_files': num_files,
+#     }
+#     return render(request, 'process.html')
 
 
 # def run_script():
